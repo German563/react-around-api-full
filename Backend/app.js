c8617508b7e1590c907d683f5771f82a908dab9b
German563 <73666252+German563@users.noreply.github.com>
2023-07-10 02:20:09 +0300

    new commit

--- Backend/app.js
+++ Backend/app.js
@@ -20,6 +20,8 @@ const { port = 3000 } = process.env;
 app.use('*', cors());
 
 mongoose.connect('mongodb://127.0.0.1:27017/aroundb');
+mongoose.set('strictQuery', false);
+
 const errorHandler = require('./middlewares/errorHandler');
 
 app.use((req, res, next) => {
