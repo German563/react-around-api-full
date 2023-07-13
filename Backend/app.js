248aaa0ba1cffa6654f0309f1333e9607cd5ecdb
German563 <73666252+German563@users.noreply.github.com>
2023-07-14 01:55:47 +0300

    new commit

--- Backend/app.js
+++ Backend/app.js
@@ -62,9 +62,6 @@ app.post(
   '/signup',
   celebrate({
     body: Joi.object().keys({
-      name: Joi.string().min(2).max(30),
-      about: Joi.string().min(2).max(30),
-      avatar: Joi.string().regex(/(http)?s?:?(\/\/[^"']*\.(?:png|jpg|jpeg|gif|png|svg))/),
       email: Joi.string().email().required(),
       password: Joi.string().required(),
     }),
